e33beec10b11e17503a39ae6befdadd724680d69
Lewis Russell <lewis6991@gmail.com>
2022-10-20 10:27:52 +0100

    remove plugin dir

--- plugin/tree-sitter-tcl.lua
+++
@@ -1,6 +0,0 @@
--- Register with nvim-treesitter so it knows to use the queries for indent and folding
-local exists, parsers = pcall(require, 'nvim-treesitter.parsers')
-if exists then
-  local parser_config = parsers.get_parser_configs()
-  parser_config.tcl = { filetype = "tcl" }
-end
