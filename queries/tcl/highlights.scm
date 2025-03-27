675f647a701e5fa0ecfe52106768dd98e6401d22
Jason Adams <j.adams@f5.com>
2025-03-27 06:38:40 -0700

    fix(highlighting): replace #lua-match? with #match? for numeric literals
    Use Tree-sitter's native #match? predicate instead of #lua-match? to ensure compatibility with editors like Zed that may not support Lua-based queries. This prevents incorrect highlighting of non-numeric simple_word nodes like "switch".

--- queries/tcl/highlights.scm
+++ queries/tcl/highlights.scm
@@ -136,7 +136,7 @@
  ] @punctuation.bracket @punctuation.delimiter
 
 ((simple_word) @number
-               (#lua-match? @number "^[0-9]+$"))
+               (#match? @number "^[0-9]+$"))
 
 ((simple_word) @boolean
                (#any-of? @boolean "true" "false"))
